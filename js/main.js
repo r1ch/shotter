77df73f6d4aa0e600acb9c9c03106cc8f6e09ef0
r1ch <rich@bradi.sh>
2020-11-18 17:04:10 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,60 +11,66 @@ const Shotter = {
 	keyLines: lines.key,
 	allLines: lines.all,
 	players : ["Alex", "Clare", "Nick", "Megan", "Rich", "Soph"],
-	characters: ["Harry", "Ron", "Hermione"]
+	characters: ["Harry", "Ron", "Hermione"],
+	template:`
+		  <div id="app">
+		    <input type = "text" v-model="epoch"> 
+		    <imbibe v-for="line in recentLines" :key="line.lineNumber" :line="line" :players="$options.players" :characters="$options.characters"></imbibe>
+		  </div>
+	`
 }
 
 const ShotterApp = Vue.createApp(Shotter)
 
 ShotterApp.component('imbibe', {
-  data: () => ({}),
-  methods: {
-    playerFromCharacter: function(character) {
-      let characterIndex = this.characters.indexOf(character)
-      if(characterIndex == -1) {return}
-      else return this.players[(characterIndex + this.line.voldeCount)%this.players.length]
-    },
-    characterFromPlayer: function(player){
-      let playerIndex = this.players.indexOf(player)
-      let inclusive = [...this.characters]
-      while(inclusive.length<this.players.length) inclusive.push("Hogwarts' Students")
-      let characterIndex =  (playerIndex - this.line.voldeCount + inclusive.length) % inclusive.length
-      return inclusive[characterIndex]
-    }
-  },
-  computed: {
-    title(){
-      let characters = Object.keys(this.line.tokens)
-      return characters.length === 1 ? characters[0] : "Multiple"
-    }
-  },
+	data: () => ({}),
+	methods: {
+		playerFromCharacter: function(character) {
+			let characterIndex = this.characters.indexOf(character)
+			if(characterIndex == -1) {return}
+			else return this.players[(characterIndex + this.line.voldeCount)%this.players.length]
+		},
+		characterFromPlayer: function(player){
+			let playerIndex = this.players.indexOf(player)
+			let inclusive = [...this.characters]
+			while(inclusive.length<this.players.length) inclusive.push("Hogwarts' Students")
+				let characterIndex =  (playerIndex - this.line.voldeCount + inclusive.length) % inclusive.length
+			return inclusive[characterIndex]
+		}
+  	},
+  	computed: {
+    		title(){
+      			let characters = Object.keys(this.line.tokens)
+      			return characters.length === 1 ? characters[0] : "Multiple"
+    		}
+  	},
 	template: `
-    <div class="card" style="width: 25rem;">
-      <div class="card-body">
-        <h5 class="card-title">{{ title }}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">
-          <span v-for = "(count, character) in line.tokens" :key="`${character}`" href="#" class="card-link">
-            {{ playerFromCharacter(character) }}
-          </span>
-        </h6>
-        <p class="card-text" >
-          <span v-for = "(text,index) in line.speech" :key="`${index}`">
-            {{ text }}
-          </span>
-        </p>
-        <div class="card-text" v-if="line.tokens.Voldemort">
-          <h6>Switch</h6>
-          <span v-for = "player in players" :key="`${player}`">
-            {{player}} : {{characterFromPlayer(player)}}<br>
-          </span>
-        </div>
-        <div class="card-text" v-if="!line.tokens.Voldemort">
-          <small v-for = "player in players" :key="`${player}`">
-          {{player}}:{{characterFromPlayer(player)}}
-          </small>
-        </div>
-      </div>
-    </div>
+	    <div class="card" style="width: 25rem;">
+	      <div class="card-body">
+		<h5 class="card-title">{{ title }}</h5>
+		<h6 class="card-subtitle mb-2 text-muted">
+		  <span v-for = "(count, character) in line.tokens" :key="`${character}`" href="#" class="card-link">
+		    {{ playerFromCharacter(character) }}
+		  </span>
+		</h6>
+		<p class="card-text" >
+		  <span v-for = "(text,index) in line.speech" :key="`${index}`">
+		    {{ text }}
+		  </span>
+		</p>
+		<div class="card-text" v-if="line.tokens.Voldemort">
+		  <h6>Switch</h6>
+		  <span v-for = "player in players" :key="`${player}`">
+		    {{player}} : {{characterFromPlayer(player)}}<br>
+		  </span>
+		</div>
+		<div class="card-text" v-if="!line.tokens.Voldemort">
+		  <small v-for = "player in players" :key="`${player}`">
+		  {{player}}:{{characterFromPlayer(player)}}
+		  </small>
+		</div>
+	      </div>
+	    </div>
 	`
 })
 
