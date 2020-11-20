6cd2e00806192686a0264e1fbd15ce0512dfc548
r1ch <rich@bradi.sh>
2020-11-20 14:07:40 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -187,12 +187,10 @@ ShotterApp.component('recent-line', {
 		<h5 class="card-title">{{ title }}</h5>
 		<h6 class="card-subtitle mb-2 text-muted">
 		  <span v-for = "(count, character) in inPlayTokens" :key="character" href="#" class="card-link">
-			<div class = "fa-2x">
 				<span class="fa-layers fa-fw">
 					<i class="fas fa-glass-cheers"></i>
 					<span class="fa-layers-counter" style="background:Tomato">{{count.local}}</span>
 				</span>
-			</div>
 			{{playerForCharacter(character)}}
 		  </span>
 		</h6>
