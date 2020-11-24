33a5ac550f8a7b1e4c798a4a39bcb70dc4482efc
r1ch <rich@bradi.sh>
2020-11-24 11:01:12 +0000

    Update versionCheck.js

--- js/versionCheck.js
+++ js/versionCheck.js
@@ -1,7 +1,11 @@
 const checkVersion = () => {
-   axios
-  .get(`${config.versionPath}?${Date.now()}`)
+   axios.request({
+      method: "get",
+      baseUrl: location.href,
+      url: "version"
+   })
   .then(({data})=>{
+    console.log(data)
     if(data.trim() != revision) location.reload();
   })
   .catch(error=>console.error)
