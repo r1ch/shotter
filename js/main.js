65357aa642f3dd38d8b98f137cb6532daeeed2ed
r1ch <rich@bradi.sh>
2021-03-10 11:46:19 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -183,7 +183,7 @@ const Shotter = {
 					score: [
 						... Object.keys(line.tokens).map(character=>{
 							let player = playerMap.find(entry=>entry.character==character)
-							console.log(player)
+							console.log(player,JSON.stringify(playerCounts))
 							player && playerCounts[player] && playerCounts[player]++
 							return {
 								player : playerCounts[player]
