2a6d2fa1e4dba41a2aabb194f4e63ba6d7bad600
r1ch <rich@bradi.sh>
2021-03-12 10:04:36 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -263,7 +263,7 @@ ShotterApp.component('map-entry',{
 	data: ()=>({}),
 	props: ["entry","colourScale"],
 	template: `
-		<li class="list-group-item list-group-item-light d-flex justify-content-between align-items-center" :style="{background:colourScale(entry.player[0])}">
+		<li class="list-group-item text-white d-flex justify-content-between align-items-center" :style="{background:colourScale(entry.player[0])}">
 			{{entry.player}}
 			<span class="badge badge-pill badge-light">
 				<i class="fas fa-bolt" v-if = "entry.character=='Harry'"></i>
